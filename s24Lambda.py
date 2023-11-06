15071c090853e520bf592b38e3e3d0e12dd0777c
Mingleemma <mingleemma09@gmail.com>
2023-11-06 14:24:37 +0000

    Clean Code & Repurpose Code for Lambda Function

--- s24Lambda.py
+++ s24Lambda.py
@@ -1,34 +1,40 @@
 # Import the required packages
 from bs4 import BeautifulSoup
 import csv
-import requests
+import json
 
 # Open an HTML file for reading
 HTMLFileToBeOpened = open("index.html", "r")
 
 # Create a CSV writer and write the header row
-f = csv.writer(open('adscdata.csv', 'w'))
+f = csv.writer(open('/tmp/adscdata.csv', 'x'))
 f.writerow(['Name', 'Session', 'Gender'])
 
-# Read the contents of the HTML file and store it in a variable
-contents = HTMLFileToBeOpened.read()
-
-# Create a BeautifulSoup object and specify the parser
-beautifulSoupText = BeautifulSoup(contents, 'html.parser')
-
-# Find all <tr> elements in the HTML, excluding the first and last row
-table = beautifulSoupText.find_all('tr')
-table = table[1:-1]
-
-# Iterate through each row in the table
-for row in table:
-    # Find all <td> elements within the current row
-    td_elements = row.find_all('td')
+def lambda_handler(event, context):
+    # Read the contents of the HTML file and store it in a variable
+    contents = HTMLFileToBeOpened.read()
     
-    # Extract and store the text from each <td> element in variables
-    value1 = td_elements[0].get_text()
-    value2 = td_elements[1].get_text()
-    value3 = td_elements[2].get_text()
+    # Create a BeautifulSoup object and specify the parser
+    beautifulSoupText = BeautifulSoup(contents, 'html.parser')
     
-    # Write the values to the CSV file
-    f.writerow([str(value1), str(value2), str(value3)])
+    # Find all <tr> elements in the HTML, excluding the first and last row
+    table = beautifulSoupText.find_all('tr')
+    table = table[1:-1]
+    
+    # Iterate through each row in the table
+    for row in table:
+        # Find all <td> elements within the current row
+        td_elements = row.find_all('td')
+        
+        # Extract and store the text from each <td> element in variables
+        value1 = td_elements[0].get_text()
+        value2 = td_elements[1].get_text()
+        value3 = td_elements[2].get_text()
+        
+        # Write the values to the CSV file
+        f.writerow([str(value1), str(value2), str(value3)])
+        
+    return{
+        'statusCode': 200,
+        'body': json.dumps('Extraction Complete')
+    }
--- scrape4lambda.py
+++ scrape4lambda.py
@@ -2,33 +2,35 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import os
+import json
 
 # Specify the URL of the web page to scrape
 url = 'https://en.wikipedia.org/wiki/Mobile_Web_Ghana'
 
-# Send an HTTP GET request to the URL and get the web page's content
-page = requests.get(url)
-
-# Parse the HTML content of the web page using BeautifulSoup
-soup = BeautifulSoup(page.text, 'html.parser')
-
-# Find the HTML table element within the web page
-table = soup.table.tbody.contents[1:]
-
-# Create a CSV writer and write the header row
-f = csv.writer(open('mwgdata.csv', 'w'))
-f.writerow(['Name', 'Link'])
-
-# Iterate through the rows in the table
-for row in table:
-    # Extract the content of the table cell in the "th" element
-    heading = row.th.string
+def lambda_handler(event, context):
+    # Send an HTTP GET request to the URL and get the web page's content
+    page = requests.get(url)
     
-    # Extract the content of the table cell in the "td" element
-    value = row.td.string
+    # Parse the HTML content of the web page using BeautifulSoup
+    soup = BeautifulSoup(page.text, 'html.parser')
     
-    # Print the extracted values
-    print(str(heading) + " - " + str(value))
+    # Find the HTML table element within the web page
+    table = soup.table.tbody.contents[1:]
     
-    # Add each artist's name and associated link to a row in the CSV file
-    f.writerow([str(heading), str(value)])
+    # Create a CSV writer and write the header row
+    f = csv.writer(open('/tmp/mwgdata.csv', 'x'))
+    f.writerow(['Name', 'Link'])
+    
+    # Iterate through the rows in the table
+    for row in table:
+        # Extract the content of the table cell in the "th" element
+        heading = row.th.string
+        
+        # Extract the content of the table cell in the "td" element
+        value = row.td.string
+    
+    return{
+        'statusCode': 200,
+        'body': json.dumps('Extraction Complete')
+    }
